d1fb0094893ad5fe76652236bd72cd8cc91ea38d
李港 <1203732857@qq.com>
2019-08-27 10:45:32 +0800

    first coommit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include"TAspeedometer.h"
+#include "TAmempool.h"
+using namespace std;
+
+void mempooltest() {
+	TAmempool pool;
+	int** x;
+	int t = 0;
+	x = new int* [10000];
+	TAtimer timer;
+	for (int m = 0; m < 10; m++) {
+		timer.start();
+		for (int j = 1; j < 200; j++) {
+			for (int i = 0; i < 10000; i++) {
+				//cout << i<<" ";
+				x[i] = (int*)pool.malloc(sizeof(int));
+			}
+			for (int i = 0; i < 10000; i++) {
+				pool.free(x[i], sizeof(int));
+			}
+		}
+
+		timer.stop();
+		cout << timer.get() << endl;
+		t += timer.get();
+	}
+	cout << endl << t / 10;
+}
+
+
+
+
+int main() {
+	mempooltest();
+
+	return 0;
+}
